107c41244582e772778fd4f4ae1519769a3456ee
Fabien Maussion <fabien.maussion@uibk.ac.at>
2016-06-19 11:05:17 +0200

    lazu properties should now keep their doc (#10)

--- salem/__init__.py
+++ salem/__init__.py
@@ -13,6 +13,7 @@ from __future__ import division
 from os import path
 from os import makedirs
 import sys
+from functools import wraps
 
 import pyproj
 
@@ -23,10 +24,12 @@ def lazy_property(fn):
     attr_name = '_lazy_' + fn.__name__
 
     @property
+    @wraps(fn)
     def _lazy_property(self):
         if not hasattr(self, attr_name):
             setattr(self, attr_name, fn(self))
         return getattr(self, attr_name)
+
     return _lazy_property
 
 wgs84 = pyproj.Proj(proj='latlong', datum='WGS84')
