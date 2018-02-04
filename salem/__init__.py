21edfe509d03eb6b15ff2cf5b2f3c815226f87d1
Fabien Maussion <fabien.maussion@uibk.ac.at>
2018-02-05 00:28:43 +0100

    Freetype issues again

--- salem/__init__.py
+++ salem/__init__.py
@@ -46,7 +46,7 @@ download_dir = path.join(cache_dir, 'downloads')
 if not path.exists(download_dir):
     makedirs(download_dir)
 
-sample_data_gh_commit = '8ca31526cce6339d69a964f2d66e2f7d42b51d34'
+sample_data_gh_commit = 'b234d1dadc71ed344dada78f29d7b6f08ff26f44'
 sample_data_dir = path.join(cache_dir, 'salem-sample-data-' +
                             sample_data_gh_commit)
 
