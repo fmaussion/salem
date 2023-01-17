938918e63d1212e1fb00aab3de919023924eaa1a
Timo Rothenpieler <timo.rothenpieler@uni-bremen.de>
2023-01-17 14:04:00 +0100

    Migrate to modern python packaging scheme

--- salem/__init__.py
+++ salem/__init__.py
@@ -10,14 +10,7 @@ from functools import wraps
 
 import pyproj
 
-try:
-    from .version import version as __version__
-except ImportError:  # pragma: no cover
-    raise ImportError('Salem is not properly installed. If you are running '
-                      'from the source directory, please instead create a '
-                      'new virtual environment (using conda or virtualenv) '
-                      'and  then install it in-place by running: '
-                      'pip install -e .')
+from .version import __version__
 
 
 def lazy_property(fn):
