85b1d21777fe6c4da136b64f966c3fe24309ce5f
Fabien Maussion <fabien.maussion@bristol.ac.uk>
2024-08-20 12:36:07 +0100

    static image update (#252)

--- salem/__init__.py
+++ salem/__init__.py
@@ -39,7 +39,7 @@ download_dir = path.join(cache_dir, 'downloads')
 if not path.exists(download_dir):
     makedirs(download_dir)
 
-sample_data_gh_commit = '454bf696324000d198f574a1bf5bc56e3e489051'
+sample_data_gh_commit = 'aa0ddda63a8025787ffe9dba3219c7926acf33cd'
 sample_data_dir = path.join(cache_dir, 'salem-sample-data-' +
                             sample_data_gh_commit)
 
