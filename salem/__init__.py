e0f4e2e10170f9a5a1f34cbc1f93c82f485539ee
Fabien Maussion <fabien.maussion@bristol.ac.uk>
2024-08-20 14:30:02 +0100

    test files

--- salem/__init__.py
+++ salem/__init__.py
@@ -39,7 +39,7 @@ download_dir = path.join(cache_dir, 'downloads')
 if not path.exists(download_dir):
     makedirs(download_dir)
 
-sample_data_gh_commit = 'aa0ddda63a8025787ffe9dba3219c7926acf33cd'
+sample_data_gh_commit = '9140f2f391aa85c079c228b4adc906fdc701638e'
 sample_data_dir = path.join(cache_dir, 'salem-sample-data-' +
                             sample_data_gh_commit)
 
