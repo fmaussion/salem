28ec571f988de39182dc96d516aa064ee95e12e5
gampnico <45390064+gampnico@users.noreply.github.com>
2026-03-25 12:16:44 +0000

    fix: update sample data hash to point to new image
    Ref: #259

--- salem/__init__.py
+++ salem/__init__.py
@@ -39,7 +39,7 @@ download_dir = path.join(cache_dir, 'downloads')
 if not path.exists(download_dir):
     makedirs(download_dir)
 
-sample_data_gh_commit = '77d826a80cf7afec213df06fb33c31491bbd049d'
+sample_data_gh_commit = '5dae2f6aa189ab9b6c6f2292c8933ef3902f0a49'
 sample_data_dir = path.join(cache_dir, 'salem-sample-data-' +
                             sample_data_gh_commit)
 
